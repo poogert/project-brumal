a8bff458d900b29355b6402de6a0970dd201da1a
Piehatesbadguys <eboypie@gmail.com>
2025-10-11 02:31:48 -0500

    Added Crouching, Leaning, and Lantern (also test mesh))
    Edgaaaa -
    Crouching and leaning are VERY barebones, both need a interpolation, and the collision on both also need some improving, but they work.
    
    The lantern is alright though, I was testing out the projection of the light on the lantern, (just press F if u wanna see ) and it has some flickering, could be cool for a gameplay feature where the lantern flickers more near the big monster guy? IDK.
    
    Oh also forgot to mention the little hallway cave mesh i put in there to test the collision and importing stuff. It works pretty well I would say, although I did have to switch the physics engine from the default godot physics to the JOLT physics engine, but I'd say that for now i dont thinkl sticking to default would do us  any favors. JOLT is goated.

---
+++ project-brumal/player/CalciumLamp.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class CalciumLamp : Node3D
+{	
+	
+	SpotLight3D CLLight; // light reference
+	
+	new bool light = false;
+	// initializing toggle boolean
+	public override void _Ready()
+	{
+		CLLight = GetNode<SpotLight3D>("SpotLight3D");// initializing the light
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{	double randomFloat = GD.Randfn(0.0, 1.0);
+		// this has to be here for the random number to update, which helps make the flicker effect
+		float lightFlicker = (float)(2 + (randomFloat * .2));
+		// ^^ the random float is added to the base brightness of the light and the intensity of the flicker 
+		// decided by the second value to the right of the randomFloat (the .2 in this case)
+		//  I loveee furriesss - Edgaaaa 
+		if(Input.IsActionJustPressed("FlashLight")){
+			light = !light;
+			// simple toggle
+		}
+		if(light){
+			CLLight.LightEnergy = lightFlicker;
+			
+		
+		}else {
+			// turn off light 
+			CLLight.LightEnergy = 0;
+		}
+	}
+	
+}
--- project-brumal/scripts/playerone.cs
+++ project-brumal/scripts/playerone.cs
@@ -9,15 +9,20 @@ public partial class playerone : CharacterBody3D
 	[Export] public float JumpVelocity = 4.5f;
 	[Export] public float sensitivity = 0.003f;
 	
+	CollisionShape3D StandCol; // stand collision root reference
+	CollisionShape3D CrouchCol; // crouch collision root reference
 	Node3D Player; // Scene root reference
 	Node3D head; // head reference
 	Camera3D camera; // camera reference
 
 	public override void _Ready()
 	{
-
+		
 		base._Ready();
+		
 		Input.MouseMode = Input.MouseModeEnum.Captured;
+		StandCol = GetNode<CollisionShape3D>("StandingCollision");  // initialize Standing collision
+		CrouchCol = GetNode<CollisionShape3D>("CrouchedCollision"); // initialize crouch collision
 		Player = GetNode<Node3D>("."); // initialize player node
 		head = GetNode<Node3D>("head_pivot"); // initialize head node
 		camera = GetNode<Camera3D>("head_pivot/head_camera"); // initialize camera node
@@ -31,10 +36,10 @@ public partial class playerone : CharacterBody3D
 		if (@event is InputEventMouseMotion mouseMotion)
 		{
 
-			head.RotateX(-mouseMotion.Relative.Y * sensitivity);
+			camera.RotateX(-mouseMotion.Relative.Y * sensitivity);
 			Player.RotateY(-mouseMotion.Relative.X * sensitivity);
-			
-			head.RotationDegrees = new Vector3(Mathf.Clamp(head.RotationDegrees.X, -90, 90),head.RotationDegrees.Y,head.RotationDegrees.Z);
+
+			camera.RotationDegrees = new Vector3(Mathf.Clamp(camera.RotationDegrees.X, -89, 89),camera.RotationDegrees.Y,camera.RotationDegrees.Z);
 			// ^^ Clamps head_Pivot(head) X so it can only rotate 180 degrees in total
 		}
    
@@ -45,7 +50,7 @@ public partial class playerone : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta)
 	{
-
+		
 		// takes the characterbody3D's velocity
 		Vector3 velocity = this.Velocity;
 		
@@ -56,6 +61,49 @@ public partial class playerone : CharacterBody3D
 			velocity += GetGravity() * (float)delta;
 
 		}
+
+		// Crouch 
+		if (Input.IsActionJustPressed("crouch"))
+		{
+			StandCol.Disabled = true;
+			// basically makes player overall hitbox smaller
+			Speed = Speed / 2.0f;
+			// makes speed half 
+			head.Transform = head.Transform.Translated(new Vector3(0,-0.8f,0));
+			// moves head pivot down thus moving camera down 
+		}
+		if(Input.IsActionJustReleased("crouch")){
+			StandCol.Disabled = false;
+			// returns overall hitbox to normal
+			Speed = Speed * 2.0f;
+			// makes speed normal
+			head.Transform = head.Transform.Translated(new Vector3(0,0.8f,0));
+			// moves head pivot up again
+		}
+		
+		/* Im ngl jessie (or whoever reads this) I know theres gotta be a better way to do this lean code, 
+		this needs a rehaul cus rn the camera when leaning can go through walls and also
+		i feel like this approach i have for reseting the camera back to default angle just sucks 
+		ass. Plus a lerp (interpolation) between the two lean positions is VERY much needed, but for 
+		now this is like super barebones and just to get a base of whats to come.
+		*/
+		if(Input.IsActionJustPressed("LeanLeft")){
+			head.RotateZ(-30f);
+			// leans left
+		}
+		if(Input.IsActionJustReleased("LeanLeft")){
+			head.RotateZ(30f);
+			// leans it back to normal
+		}
+		if(Input.IsActionJustPressed("LeanRight")){
+			head.RotateZ(30f);
+			// leans right
+		}
+		if(Input.IsActionJustReleased("LeanRight")){
+			head.RotateZ(-30f);
+			// leans it back to normal
+		}
+
 
 		// if space pressed, add jump velocity
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
