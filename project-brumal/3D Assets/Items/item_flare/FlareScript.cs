fcea65ea5dbd7c421a9f30f5f14650b303fd7d5b
poogert <jesse.llamas.poog@gmail.com>
2025-12-03 22:01:02 -0600

    buggy but throwable flare now.

--- project-brumal/3D Assets/Items/item_flare/FlareScript.cs
+++ project-brumal/3D Assets/Items/item_flare/FlareScript.cs
@@ -1,40 +1,65 @@
 using Godot;
 using System;
-/*
+
 public partial class FlareScript : Node3D
 {
 
 	[Export] PackedScene throwable;
 
-	private CharacterBody3D player;
-    private Node3D camera;
+	private Node player;
+	Node3D playerNode3D;
+	CharacterBody3D playerBody;
+	private Node3D camera;
+
 	
-	public int flareCount = 5;
+	
+	public int flareCount = 200;
 	public override void _Ready()
-    {
-        player = GetNode<CharacterBody3D>("World/Player");
-		camera = GetNode<Node3D>("World/Player/CharacterBody3D/head_pivot/head_adjustments/head_camera");
-    }
+	{
+	
+	}
 
 	public override void _Process(double delta)
-    {
+	{
 		if ( Input.IsActionJustPressed("leftclick") && flareCount > 0 )
 		{
+
+
+			player = GetTree().GetFirstNodeInGroup("Player");
+			playerNode3D = player as Node3D;
+			playerBody = playerNode3D as CharacterBody3D;
+
+			camera = player.GetNode<Node3D>("CharacterBody3D/head_pivot");
+
+			/* 
+				left off here -> issues 
+					player position static
+					when throwing the flare it doesnt detect y axis only z/x 
+			*/ 
+			GD.Print("throwing!");
+
+			GD.Print("player position : " + playerNode3D.GlobalPosition);
+
 			Node flare = throwable.Instantiate(); 
+			Node3D flare3D = flare as Node3D;
+
 			GetTree().Root.AddChild(flare);
 
-			// need a reference to the player character within the world scene
-			Vector3 spawn = player.GlobalTransform.Origin; 
-
-			// once player reference made, grab camera
+			Vector3 spawnPosition = playerNode3D.GlobalPosition;
 			Vector3 throwDirection = camera.GlobalTransform.Basis.Z.Normalized() * -1.0f;
 
+			spawnPosition.Y += 1.5f; 
+			spawnPosition += throwDirection * 0.5f; 
+
+			flare3D.GlobalPosition = spawnPosition; 
+
 			// the throwable item currently has rigidbody as child
-			RigidBody3D rigidBody3D = flare.GetNode<RigidBody3D>("RigidBody3D");
-			rigidBody3D.ApplyImpulse(throwDirection * 25.0f);
+			RigidBody3D rigidBody3D = flare3D.GetNode<RigidBody3D>("RigidBody3D");
+			rigidBody3D.ApplyImpulse(throwDirection * 16.0f);
 
 			flareCount -= 1;
+			GD.Print("flares : " + flareCount);
+
 		}
-    }
+	}
 }
-*/
