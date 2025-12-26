7435f26f2d4cba6559dde9106935b00da79833c5
Shinomations <aaronburch23@gmail.com>
2025-12-26 03:43:34 -0600

    Aaron_UISpecialPlace
    so within this update i created a new testing area for me in the files. Then within this area i made multiple things, 1 script for item switching and hotbar revealing code. Then a UI world where my UI testing happens and then finally my pixel art for those icons. I also changed some project settings, first i changed the texture rendering to nearest, which makes the textures for my stuff not blurry. lastly i added a input for the button Tab (Temp change for testing purposes)

---
+++ project-brumal/z_AaronsTestingGrounds/hotbarButtonManager.gd
@@ -0,0 +1,37 @@
+extends MarginContainer
+
+@export var menu_screen: VBoxContainer
+@export var buttonPic: Button
+@export var buttonFlare: Button
+@export var buttonLight: Button
+@export var buttonGrapple: Button
+var buttonArray: Array[Button] = []
+
+func _ready():
+	buttonArray = [buttonPic,buttonFlare,buttonLight,buttonGrapple]
+	for button in buttonArray:
+		button.disabled = true
+
+func enable_only(target: Button):
+	for button in buttonArray:
+		if button:
+			button.disabled = (button != target)
+			print(Input,"changed",buttonArray, "Scale but not any other")
+
+func _process(_delta):
+	if Input.is_action_just_pressed("itemone") and buttonPic:
+		enable_only(buttonPic)
+
+	if Input.is_action_just_pressed("itemtwo") and buttonFlare:
+		enable_only(buttonFlare)
+
+	if Input.is_action_just_pressed("itemthree") and buttonLight:
+		enable_only(buttonLight)
+
+	if Input.is_action_just_pressed("itemfour") and buttonGrapple:
+		enable_only(buttonGrapple)
+
+	if Input.is_action_just_pressed("toggle_menu"):
+		menu_screen.visible = !menu_screen.visible
+		print(menu_screen.visible)
+		
